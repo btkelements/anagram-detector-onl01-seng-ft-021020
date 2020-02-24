1d9809d5846d103e74d1988b7711325a4f4f1207
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-24 01:41:39 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,15 @@
 # Your code goes here!
 
 class Anagram
-  attr_acessor :word
+  attr_acessor :w
   
-  def initialize()
+  def initialize(w)
+    @w = w
+  end
+  
+  def match(w_array)
+    w_array.select do |w|
+      w.split("").sort == @w.split("").sort
+    end
+  end
+end
